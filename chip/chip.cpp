18a0c03fedc6ea4ffad39c7def8f59f40d9fbf72
Yhodhan <mscuderi855@gmail.com>
2024-06-12 18:13:58 -0300

    Add remaining fields and initialization

--- chip/chip.cpp
+++ chip/chip.cpp
@@ -1,6 +1,12 @@
 #include "chip.h"
 
-Chip::Chip(): memory(std::vector<byte> (0x1000, 0x1)) {} 
+Chip::Chip(): rt(0), rd(0), sp(0), i(0), pc(0x0200),
+              stack(std::vector<word> (16, 0)),
+              registers(std::vector<byte> (16, 0)),
+              memory(std::vector<byte> (0x1000, 0)),
+              keyboard(std::vector<bool> (16, 0)),              
+              screen(std::vector<std::vector<byte>>(64, std::vector<byte> (32, 0)))
+             {} 
 Chip::~Chip() {}
 
 void Chip::print_mem() {
--- chip/chip.h
+++ chip/chip.h
@@ -1,15 +1,19 @@
 #ifndef CHIP_H
 #define CHIP_H
+
 #include <cstdint>
 #include <iostream>
 #include <ostream>
 #include <vector>
 
 typedef uint8_t byte;
+typedef uint16_t word;
 
-// -----------------
-//  CHIP8 EMULATOR 
-// -----------------
+/*
+ =================
+   CHIP8 EMULATOR 
+ =================
+*/
 class Chip {
 public:
   Chip();
@@ -18,7 +22,29 @@ public:
   void print_mem();
 
 private:
+  /*
+  ======================================================
+    - rt, rd: Time and Delay registers.
+    - i: Instruction register.
+    - pc: Program counter.
+    - sp: Stack pointer.
+    - Stack: It has up to 16 addresses of space, Chip8 only allows up to 16 sub routines calls.
+    - Memory: From 0x000 to 0x01FF Program / Data Space.
+    - Registers: Chip8 has 16 8 bits registers, 0xF register usually is used to store flags.
+    - Keyboard: It has 16-key hexadecimal layout.
+  ======================================================
+  */
+
+  byte rt;
+  byte rd;
+  byte sp;
+  word i;
+  word pc;
+  std::vector<word> stack; 
   std::vector<byte> memory;
+  std::vector<byte> registers; 
+  std::vector<bool> keyboard; 
+  std::vector<std::vector<byte>> screen;
 };
 
 #endif
